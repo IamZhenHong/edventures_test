29c3cc14d17d60eece149034c8d8450af136aa58
IamZhenHong <109903639+IamZhenHong@users.noreply.github.com>
2024-10-20 22:19:38 +0800

    Implement file upload function
    feat
    
    Implement manual file upload for user

--- api/urls.py
+++ api/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from .views import ChatbotResponseView
+from .views import ChatbotResponseView, AddDocumentsView, add_document
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('query/', ChatbotResponseView.as_view(), name='chatbot-response'),
+    path('add-document_template/', add_document, name='add-document'),
+    path('add-document/', AddDocumentsView.as_view(), name='add-document'),
 
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    
--- api/views.py
+++ api/views.py
@@ -4,6 +4,10 @@ from rest_framework import status
 from .services.document_retriever import find_relevant_document
 from .services.pdf_processing import pdf_processing
 from .services.csv_processing import csv_processing
+from .models import Document
+import os
+from django.conf import settings
+from django.shortcuts import render
 
 class ChatbotResponseView(APIView):
     """
@@ -42,3 +46,41 @@ class ChatbotResponseView(APIView):
             raise ValueError("Unsupported document type.")
 
         return response
+    
+class AddDocumentsView(APIView):
+
+    def post(self, request):
+        """Handle POST requests to add documents."""
+        uploaded_file = request.FILES.get('file')
+        description = request.data.get('description')
+
+        if uploaded_file and description:
+            file_name = uploaded_file.name
+            file_type = uploaded_file.content_type  # Get the full MIME type
+
+            # Extract the file extension (either pdf or csv)
+            if 'pdf' in file_type:
+                file_type = 'pdf'
+            elif 'csv' in file_type:
+                file_type = 'csv'
+            else:
+                return Response({"error": "Invalid file type. Only PDF and CSV files are allowed."}, 
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            # Create and save the document instance
+            document = Document(
+                file_name=file_name,
+                file_type=file_type,
+                description=description,
+                file=uploaded_file  # Populate the FileField with the uploaded file
+            )
+            document.save()
+
+            return Response({"message": "Document added successfully."}, 
+                            status=status.HTTP_201_CREATED)
+
+        return Response({"error": "File and description are required."}, 
+                        status=status.HTTP_400_BAD_REQUEST)
+def add_document(request):
+    template_name = 'api/add_document.html'
+    return render(request, template_name)
